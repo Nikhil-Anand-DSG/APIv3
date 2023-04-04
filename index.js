66757c7b65e14e2c17b3733cc9b9b8d5b893605a
Nik Anand <nikhil.anand@dcsg.com>
2023-04-04 16:23:24 -0400

    pulled both player info and stats in 1 req

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const fs = require("fs");
 const fetch = require("node-fetch");
-const {response} = require("express");
 
 app.get('/listPlayers', function (req, res) {
     fs.readFile( __dirname + "/" + "players.json", 'utf8', function (err, data) {
@@ -26,13 +25,14 @@ app.get("/player/:name/seasonStats", async (req, res) => {
             method: 'GET',
         };
         let playerInfo = await fetch("https://balldontlie.io/api/v1/players?search="+req.params.name, requestOptions);
-        let playerID = await playerInfo.json();
-        playerID = playerID.data[0].id;
+        let playerInfoJson = await playerInfo.json();
+        let playerID = playerInfoJson.data[0].id;
 
         const playerSeasonStatsReq = await fetch("https://balldontlie.io/api/v1/season_averages?player_ids[]="+playerID, requestOptions);
         const playerSeasonStats = await playerSeasonStatsReq.json();
 
-        res.write(JSON.stringify(playerSeasonStats.data[0]));
+
+        res.write("{\""+req.params.name+"\": ["+JSON.stringify(playerInfoJson.data[0]).substring(0,JSON.stringify(playerInfoJson.data[0]).length-1)+", "+JSON.stringify(playerSeasonStats.data[0]).substring(1)+"]}");
         res.end();
 });
 
@@ -45,10 +45,9 @@ app.get("/player/:name/position", async (req, res) => {
     let finalStat = await stat.json();
     finalStat = finalStat.data[0].position;
 
-    res.write(finalStat);
+    res.write(JSON.stringify(finalStat));
     res.end();
 });
-
-const server = app.listen(8081, function () {
+app.listen(8081, function () {
     console.log("Example app listening at http://127.0.0.1:8081");
 });
