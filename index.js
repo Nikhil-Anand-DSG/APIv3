af4ca825e59ffa284241e576e6a4d08e1da5b955
Nik Anand <nikhil.anand@dcsg.com>
2023-04-06 14:42:55 -0400

    pulled both player info and stats in 1 req

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
 const express = require('express');
 const app = express();
-const fs = require("fs");
 const fetch = require("node-fetch");
-
-app.get('/listPlayers', function (req, res) {
-    fs.readFile( __dirname + "/" + "players.json", 'utf8', function (err, data) {
-        console.log( data );
-        res.end( data );
-    });
-})
-
-app.get('/player/:name', function (req, res) {
-    // get player info based on name
-    fs.readFile( __dirname + "/" + "players.json", 'utf8', function (err, data) {
-        const players = JSON.parse(data);
-        const player = players[req.params.name];
-        console.log( player );
-        res.end( JSON.stringify(player));
-    });
-});
 
 app.get("/player/:name/seasonStats", async (req, res) => {
         const requestOptions = {
@@ -36,18 +18,6 @@ app.get("/player/:name/seasonStats", async (req, res) => {
         res.end();
 });
 
-app.get("/player/:name/position", async (req, res) => {
-    const requestOptions = {
-        method: 'GET',
-    };
-    let stat = await fetch("https://balldontlie.io/api/v1/players?search="+req.params.name, requestOptions);
-
-    let finalStat = await stat.json();
-    finalStat = finalStat.data[0].position;
-
-    res.write(JSON.stringify(finalStat));
-    res.end();
-});
 app.listen(8081, function () {
     console.log("Example app listening at http://127.0.0.1:8081");
 });
